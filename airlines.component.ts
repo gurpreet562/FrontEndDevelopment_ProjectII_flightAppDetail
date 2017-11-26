0f6d8ac12b9a159fa6a45eb48c7903242cc84489
gurpreet562 <kgurpreet562@gmail.com>
2017-11-26 16:32:00 +0530

    Update airlines.component.ts

--- airlines.component.ts
+++ airlines.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
-import { MarvelService } from "../services/marvel.service";
+import { MarvelService } from "../services/airline.service";
 
 @Component({
     selector:"super-heroe",
